45a60eb3d342c53e606039c22c0b3c9e5a79095b
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-02 20:21:09 -0300

    Aprimora a lista de tarefas

--- script-crud.js
+++ script-crud.js
@@ -2,8 +2,14 @@ const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 const ulTarefas = document.querySelector(".app__section-task-list");
+const paragrafoDescricaoTarefa = document.querySelector(
+  ".app__section-active-task-description"
+);
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
+
 
 function atualizarTarefas() {
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
@@ -37,7 +43,6 @@ function criarElementoTarefa(tarefa) {
       atualizarTarefas();
     }
   };
-
   const imagemBotao = document.createElement("img");
   imagemBotao.setAttribute("src", "/imagens/edit.png");
   botao.append(imagemBotao);
@@ -45,6 +50,25 @@ function criarElementoTarefa(tarefa) {
   li.append(svg);
   li.append(paragrafo);
   li.append(botao);
+
+  li.onclick = () => {
+    document
+      .querySelectorAll(".app__section-task-list-item-active")
+      .forEach((elemento) => {
+        elemento.classList.remove("app__section-task-list-item-active");
+      });
+    if (tarefaSelecionada === tarefa) {
+      paragrafoDescricaoTarefa.textContent = "";
+      tarefaSelecionada = null;
+      liTarefaSelecionada = null;
+      return;
+    }
+    tarefaSelecionada = tarefa;
+    liTarefaSelecionada = li
+    paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+
+    li.classList.add("app__section-task-list-item-active");
+  };
 
   return li;
 }
@@ -71,3 +95,11 @@ tarefas.forEach((tarefa) => {
   const elementoTarefa = criarElementoTarefa(tarefa);
   ulTarefas.append(elementoTarefa);
 });
+
+document.addEventListener('FocoFinalizado', () => {
+  if (tarefaSelecionada && liTarefaSelecionada) {
+    liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+    liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+    liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+  }
+})
